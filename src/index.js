a07fc123eaaad26ce55581c3c4004b607f88dfe9
itsjzt <saurabh.19@outlook.com>
2018-03-02 17:41:13 +0530

    done !!!

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
-const express = require('express')
-const { isUnix, isNatural, toNatural, toUnix } = require('./time')
-const app = express()
+const express = require("express");
+const { isUnix, isNatural, toNatural, toUnix } = require("./time");
+const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get("/", (req, res) => res.send("Hello dude!"));
 
-app.get('/:timestamp', (req, res) => {
-  const { timestamp } = req.params
+app.get("/:timestamp", (req, res) => {
+  const { timestamp } = req.params;
 
   if (isUnix(timestamp)) {
-    res.json({unix: timestamp, natural: toNatural(timestamp)})
+    res.json({ unix: timestamp, natural: toNatural(timestamp) });
+  } else if (isNatural(timestamp)) {
+    res.json({ unix: toUnix(timestamp), natural: timestamp });
+  } else {
+    res.json({ unix: null, natural: null });
   }
-  else if (isNatural(timestamp)) {
-    res.json({ unix: toUnix(timestamp), natural: timestamp })
-  }
-  else {
-    res.json({unix: null, natural: null})
-  }
-})
+});
 
-const port = process.env.PORT || 4567
-app.listen(port, () => console.log(`Listening on port ${port}`))
+const port = process.env.PORT || 4567;
+app.listen(port, () => console.log(`Listening on port ${port}`));
--- src/time.js
+++ src/time.js
@@ -1,20 +1,18 @@
-const moment = require('moment')
+const moment = require("moment");
 
 // any positive number can be a unix timestamp
-exports.isUnix = (timestamp)  => {
-  return Number(timestamp) >= 0
-}
+exports.isUnix = timestamp => {
+  return !isNaN(timestamp);
+};
 
-exports.isNatural = (timestamp) => {
-  return moment(timestamp, 'MMMM DD YYYY').isValid()
-}
+exports.isNatural = timestamp => {
+  return moment(timestamp, "MMMM DD,YYYY").isValid();
+};
 
-exports.toNatural = (unixtime) => {
-  return moment.unix(unixtime).format("MMMM DD YYYY")
-}
+exports.toNatural = unixtime => {
+  return moment.unix(unixtime).format("MMMM DD,YYYY");
+};
 
-exports.toUnix = (timestamp) => {
-  const ts = moment(timestamp, "MMMM DD YYYY").unix()
-  return moment.unix(ts)
-}
-
+exports.toUnix = timestamp => {
+  return new Date(timestamp).getTime() / 1000;
+};
